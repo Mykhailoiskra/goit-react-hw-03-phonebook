f133d17ae6941a31fad07cc00dfeb48c5e510204
Mykhailo Iskra <iskramichel@gmail.com>
2020-12-19 14:54:27 +0200

    adds localstorage and makes Contact as a separate component

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,23 +1,19 @@
 import s from "./ContactList.module.css";
 import PropTypes from "prop-types";
+import Contact from "../Contact/Contact.jsx";
 
 const ContactList = ({ contacts, onDeleteContact }) => {
   return (
     <ul className={s.contactList}>
       {contacts.map(({ id, name, number }) => (
         <li key={id} className={s.contactList__item}>
-          <div className={s.contactList__text}>
-            <p className={s.contactList__name}>{name}</p>
-            <p className={s.contactList__number}>Tel: {number}</p>
-          </div>
-          <button
-            className={s.contactList__deleteBtn}
-            onClick={() => {
+          <Contact
+            name={name}
+            number={number}
+            onDelete={() => {
               onDeleteContact(id);
             }}
-          >
-            Delete
-          </button>
+          />
         </li>
       ))}
     </ul>
